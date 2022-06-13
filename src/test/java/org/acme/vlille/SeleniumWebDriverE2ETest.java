b938adf5e257e7e66dfea7e517dd3d6f645d9d75
jcsim1024 <38916993+jcsim1024@users.noreply.github.com>
2022-06-13 09:31:51 +0000

    clean selenium  web driver test

--- src/test/java/org/acme/vlille/SeleniumWebDriverE2ETest.java
+++ src/test/java/org/acme/vlille/SeleniumWebDriverE2ETest.java
@@ -3,10 +3,9 @@ package org.acme.vlille;
 import java.net.URL;
 import java.time.Duration;
 import java.util.concurrent.TimeUnit;
-import java.util.function.Function;
 
 
-import org.apache.commons.lang3.math.NumberUtils;
+import lombok.extern.slf4j.Slf4j;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -14,7 +13,6 @@ import org.junit.jupiter.api.Test;
 
 
 
-import io.quarkus.logging.Log;
 import io.quarkus.test.junit.QuarkusTest;
 
 import org.openqa.selenium.By;
@@ -28,20 +26,20 @@ import org.openqa.selenium.support.ui.Wait;
 
 
 /**
- * /!\ /!\ /!\ None deterministic test. Require proper configuration and
- * VLilledataset api available. /!\ /!\ /!\
- * 
  * Run with Junit 5
  * 
  * @author jcsimonnet
  *
  */
 @QuarkusTest
+@Slf4j
 public class SeleniumWebDriverE2ETest {
 
 
 	public static final String INPUT_VLILLE_STATION_SEARCH_BAR = "input-vlille-station-search-bar";
-	public static final String INPUT_VLILLE_STATION_FIRST_ID = "vlille-station-id-1";
+	public static final String TEST_DATASET_STATION_NAME = "DELESALLE MEDIATHEQUE";
+	public static final String CSS_SELECTOR_FIRST_OCC_STATION_NAME = "#stations tr td[data-name='station-name'].ng-binding ";
+
 
 	private WebDriver driver;
 	private Wait<WebDriver> wait;
@@ -60,43 +58,41 @@ public class SeleniumWebDriverE2ETest {
 		chromeOptions.setHeadless(true);
 		chromeOptions.addArguments("--no-sandbox");
 		this.driver = new RemoteWebDriver(new URL(SELENIUM_URL), chromeOptions);
+		driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
 		wait = new FluentWait<WebDriver>(driver)//
 			.withTimeout(Duration.ofSeconds(30))//
 			.pollingEvery(Duration.ofSeconds(3))//
-			.ignoring(NoSuchElementException.class);
+			.ignoring(NoSuchElementException.class)
+				.withMessage(()->{
+					log.error(driver.getPageSource());
+					return "wait failed NoSuchElementException";
+				});
 	}
 
 	@Test
-	public void Login() throws Exception {
+	public void should_filter_station_by_name() {
 		debugStep();
 
-		// Can the driver reach the page inside the container
 		driver.get(URL_TEST_APP);
-		driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
 		
 		debugStep();
 
-		String str = driver.getPageSource();
-		
-		Assertions.assertTrue(str.contains("placeholder=\"RUE ROYAL , FLANDRE...\""),
-				"Selenium couldn't reach container, try curl");
 
-		// Test AngularJs is downloaded on client selenium chrome.
-		fluentFind(wait, By.id(INPUT_VLILLE_STATION_SEARCH_BAR)).click();
+		Assertions.assertTrue(driver.getPageSource().contains("placeholder=\"RUE ROYAL , FLANDRE...\""),
+				"Selenium couldn't reach container, try curl inside it");
+
+		fluentFind(wait, By.id(INPUT_VLILLE_STATION_SEARCH_BAR)).sendKeys(TEST_DATASET_STATION_NAME);
 		debugStep();
-		fluentFind(wait, By.id(INPUT_VLILLE_STATION_SEARCH_BAR)).sendKeys("DELESALLE MEDIATHEQUE");
-		debugStep();
-		driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
 
-		// Get the row value.
-		String nbVelodispo = fluentFind(wait, By.id("vlille-station-id-1")).getText();
 
-		Assertions.assertTrue(NumberUtils.isParsable(nbVelodispo));
+		String result = fluentFind(wait, By.cssSelector(CSS_SELECTOR_FIRST_OCC_STATION_NAME)).getText();
+
+		Assertions.assertEquals(TEST_DATASET_STATION_NAME, result);
 
 	}
 
 	private void debugStep() {
-		Log.debug(	"\n E2E step° " + debugId++);
+		log.debug(	"\n E2E step° " + debugId++);
 	}
 
 	protected WebElement fluentFind(Wait<WebDriver> wait, By by) {
